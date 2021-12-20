ab3a9678a4ba81284a682926812065d3cec23626
Igor Henrique <ihsrp@cin.ufpe.br>
2021-12-20 19:26:09 -0300

    usando lib chromaprint e acoustid

--- a.py
+++ a.py
@@ -2,9 +2,13 @@ from acoustid import lookup, fingerprint_file
 import chromaprint
 import os
 import audioread
+import wikipedia
+import requests
+
 
 API_KEY = 'NMMRWI3vOJ'
 MAX_AUDIO_LENGTH = 120  # Seconds.
+LYRICS_URL = 'https://api.lyrics.ovh/v1/'
 
 def generate_fingerprint(samplerate, channels, pcmiter, maxlength=MAX_AUDIO_LENGTH):
   """Fingerprint audio data given its sample rate and number of
@@ -30,16 +34,30 @@ def generate_fingerprint(samplerate, channels, pcmiter, maxlength=MAX_AUDIO_LENG
   except chromaprint.FingerprintError:
     print('fodeu')
 
-path = os.path.abspath(os.path.expanduser('./descubra.mp3'))
+path = os.path.abspath(os.path.expanduser('mp3/descubra.mp3'))
 
 with audioread.audio_open(path) as f:
   duration = f.duration
   fp = generate_fingerprint(f.samplerate, f.channels, iter(f))
 # duration, fingerprint = fingerprint_file('./descubra.mp3')
-print(duration, int.from_bytes(fp, "little"))
+# print(duration, int.from_bytes(fp, "little"))
 
 result = lookup(API_KEY, fp, duration)
 
+found = False
+i = 0
+while not found:
+  first_result = result['results'][0]['recordings'][i]
+  song_name = first_result['title']
+  artist_name = first_result['artists'][0]['name']
+  if first_result and song_name and artist_name:
+    print('{} - {}'.format(song_name, artist_name))
+    result = wikipedia.page(artist_name + ' artist')
+    print(result.content)
+    lyrics_query = LYRICS_URL + '/' + artist_name + '/' + song_name
+    res = requests.get(lyrics_query)
+    print(res.json()['lyrics'])
+    found = True
+  i+=1
 
-print(result)
-# api key: NMMRWI3vOJ
+    
--- main.py
+++ main.py
@@ -9,5 +9,8 @@ file_mono_path = os.path.join(MONO_FOLDER, file)
 samplerate, samples = wavfile.read(file_mono_path)
 
 cu = fingerprint(samples)
+mapper = {}
+for hash, offset in cu:
+  mapper[hash.upper()] = offset
 
-print(cu)
+print(mapper)
